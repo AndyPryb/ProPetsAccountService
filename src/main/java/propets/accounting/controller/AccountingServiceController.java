cba3ebf5fd1a745a1f41408bd0470d587edf0a02
Andy <andystarrrr@gmail.com>
2020-10-03 15:13:39 +0300

    package name fixed

--- src/main/java/propets/accounting/controller/AccountingServiceController.java
+++ src/main/java/propets/accounting/controller/AccountingServiceController.java
@@ -1,4 +1,4 @@
-package propets.acconting.controller;
+package propets.accounting.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
