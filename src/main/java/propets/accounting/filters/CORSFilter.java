d968040995ceeaa02a8166dc1a5abf20e67be034
AndyPryb <Punks7Not3Dead>
2020-11-28 15:51:02 +0200

    fixing cors

--- src/main/java/propets/accounting/filters/CORSFilter.java
+++ src/main/java/propets/accounting/filters/CORSFilter.java
@@ -18,23 +18,23 @@ import org.springframework.stereotype.Service;
 public class CORSFilter implements Filter {
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		
 		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) resp;
-		
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		response.addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
-		
-		if ("OPTIONS".equals(request.getMethod())) {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization, X-Token, X-ServiceName, X-Login");
+		response.addHeader("Access-Control-Expose-Headers", "*");
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_ACCEPTED);
+			return;
 		}
-		
 		chain.doFilter(request, response);
-		
 	}
-	
+
 //    @Override
 //    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 //            throws ServletException, IOException {
