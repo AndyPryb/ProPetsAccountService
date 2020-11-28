5ecb860592f8e9cc4b70d6049e493efe07876bb3
AndyPryb <Punks7Not3Dead>
2020-11-28 21:10:18 +0200

    cors

--- src/main/java/propets/accounting/filters/CORSFilter.java
+++ src/main/java/propets/accounting/filters/CORSFilter.java
@@ -34,20 +34,4 @@ public class CORSFilter implements Filter {
 		}
 		chain.doFilter(request, response);
 	}
-
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Headers", "Authorization, content-type, X-Token");
-//        response.addHeader("Access-Control-Expose-Headers", "X-Token");
-//        if ("OPTIONS".equals(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            filterChain.doFilter(request, response);
-//        }
-//    }
-
 }
